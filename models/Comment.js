00a3c8cd3114bc6c24fd4ad80dc3ea05097b5324
unknown <chesebrojared@gmail.com>
2022-01-05 08:21:57 -0800

    Comment and Post models

--- models/Comment.js
+++ models/Comment.js
@@ -3,6 +3,44 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-Comment.init({})
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
 
 module.exports = Comment;
